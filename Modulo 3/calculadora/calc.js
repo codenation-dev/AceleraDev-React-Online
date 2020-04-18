10abe0d99fbbdafff165cfda18bbb2d724b15a86
Henrique Jensen <henriquejensen@hotmail.com>
2020-04-18 15:14:33 -0300

    module 3: calculator

---
+++ Modulo 3/calculadora/calc.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  if (num2 == 0) {
+    return 0;
+  }
+  return num1 / num2;
+}
+
+module.exports = {
+  soma: add,
+  subtracao: sub,
+  multiplicacao: mult,
+  divisao: div
+};
